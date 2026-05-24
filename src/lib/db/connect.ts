ee3272120c48ff8796acd735cd4e0f4fa4c5d9a7
hafiz <hafizulzayltd@gmail.com>
2026-05-24 16:47:09 +0600

    MONGODB_URI

--- src/lib/db/connect.ts
+++ src/lib/db/connect.ts
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define MONGODB_URI in .env');
-}
+// if (!MONGODB_URI) {
+//   throw new Error('Please define MONGODB_URI in .env');
+// }
 
 interface MongooseCache {
   conn: typeof mongoose | null;
